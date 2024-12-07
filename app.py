70f017b9030ceb6442e565243c4d4b02fbfd6652
Borja <Borja5423@gmail.com>
2024-12-07 12:09:49 +0100

    Añadida la ruta FLASK

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ def excel():
 def excel7():
     return '¡Excel7!'
 
+@app.route('/hello_flask')  # Nueva ruta para la página principal
+def hello_flask():
+    return '¡Bienvenido a mi aplicación Flask!'
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
